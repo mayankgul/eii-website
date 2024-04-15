79552d3084f872b924086b4aea9ca0f32cd5f487
Mayank Gulati <sagax.dg@gmail.com>
2024-04-15 20:27:00 +0530

    Completed navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Navbar } from "./components/navbar";
+
 const App = () => {
-  return <div>EII</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Navigate to="home" />} />
+        <Route path="home" element={<Home />} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+
 export const Home = () => {
+  useEffect(() => {
+    document.title = "EII | IIT Gandhinagar";
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
